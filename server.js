b95dd0faf24beec9e056671deff79cfd08949390
gal schwimmer <test@testadfasdfdafafdsafsdfds.com>
2016-04-21 16:58:11 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
+
+var endpoint = "http://" + process.env.DYNAMODB_HOST + ':8000';
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "us-west-2",
+  accessKeyId : "aaa",
+  secretAccessKey: "aa",
+  endpoint:  endpoint
+});
+
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName : "Movies3",
+    KeySchema: [       
+        { AttributeName: "year", KeyType: "HASH"},  //Partition key
+        { AttributeName: "title", KeyType: "RANGE" }  //Sort key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "year", AttributeType: "N" },
+        { AttributeName: "title", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    }
+};
+
+dynamodb.createTable(params, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
+
